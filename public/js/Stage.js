3da64ba26db82067479448a24620a50563470781
Jong Geon Choi <54804037+zofqofhtltlm8015@users.noreply.github.com>
2019-11-22 03:33:29 +0900

    많은 게 추가됬다!

--- public/js/Stage.js
+++ public/js/Stage.js
@@ -1,21 +1,22 @@
 
 let img = document.getElementById('Ship_comes_korea');
-var mood =1;
+var mood = 1;
 setInterval(function(){
     axios({
         method: 'POST',
-        url : 'http://172.26.0.131:80/ship',
+        url : 'http://172.26.0.131/ship',
         data:
         {
             "mood" : mood
         }
     }).then(function (response)
     {
-        
         $("#Time_comes_Korea").val(`위험 ${Date()} 에 어떤 배가 영토에 불법으로 들어갔습니다.`);
-
         console.log(response);
-        img.src = response.data;
+        console.log("data:image/gif;base64,"+ response.data);
+        // =response.data
+        // img.src ="data:image/gif;base64,"+ response.data
+        img.src="data:image/gif;base64,"+ response.data
     }).catch(function (error)
     {
         console.log('error');
@@ -32,7 +33,7 @@ setInterval(function(){
     
     axios({
         method: 'POST',
-        url : 'http://172.26.0.131:80/ship',
+        url : 'http://172.26.0.131/ship',
         data:
         {
             "mood" : mood
