950c3603258b89e802abb10d01d2b7ce466cd209
H_Apple |A.S.A.P| <39252325+HumanAdjust@users.noreply.github.com>
2019-11-21 14:47:09 +0900

    서버 기본 값 수정

--- server.js
+++ server.js
@@ -1,33 +1,50 @@
+var createError = require('http-errors');
 var express = require('express');
-var logger = require('morgan');
-var favicon = require('serve-favicon');
 var path = require('path');
-var app = express();
 var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 var session = require('express-session');
+var app = express();
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));  //favicon 처리 맛깔나게 해버렷!!!
-app.use(logger('dev'));  //로깅 처리
-app.use(express.static('public'));  //기본 경로는 public
-app.use(express.urlencoded({ extended: true }));  //body 데이터 파싱
-app.use(cookieParser());  //쿠키를 사용해 버리겠어!
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'react');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    key: 'sid', // 세션키
-    secret: 'secret', // 비밀키
-    cookie: {
-        maxAge: 1000 * 60 * 60 // 쿠키 유효기간 1시간
-    }
+  resave: false,
+  saveUninitialized: false,
+  key: 'sid', // 세션키
+  secret: 'secret', // 비밀키
+  cookie: {
+    maxAge: 1000 * 60 * 60 // 쿠키 유효기간 1시간
+  }
 })); //세션 처리
 
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/exchange', exchangeRouter);
+app.use('/admin', adminRouter);
 
-app.use('/', cording);
-app.use('/user', cording);
-app.use('/game', cording);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-app.listen(80, function () {
-    console.log('80번 포트에서 대기중');
-}); 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+module.exports = app;
