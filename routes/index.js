ebac78082d7ae864cb390e49e47b5a7fae57f673
Jong Geon Choi <54804037+zofqofhtltlm8015@users.noreply.github.com>
2019-11-21 21:52:41 +0900

    Stage.ejs 등등 해결

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    port: 3308,
-    database: 'security'
-});
+// var mysql = require('mysql');
+// var connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '1234',
+//     port: 3308,
+//     database: 'security'
+// });
 
-connection.connect();
+// connection.connect();
 
 router.get('/', function(req, res){
     res.render('../views/login.ejs');
@@ -20,23 +20,23 @@ router.get('/stage', function(req, res){
 });
 
 router.post('/check_login', function (req, res) {
-    var username = req.body.username;
-    var password = req.body.password;
-    if (username && password) {
-        connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function (error, results, fields) {
-            if (results.length > 0) {
-                req.session.loggedin = true;
-                req.session.username = username;
-                res.send('<script type="text/javascript">alert("로그인에 성공하셨습니다!"); document.location.href="/stage";</script>');
-                res.end();
-            } else {
-                res.send('<script type="text/javascript">alert("ID 또는 비번이 틀렸습니다!"); document.location.href="/";</script>');
-            }
-            res.end();
-        });
-    } else {
-        res.send('<script type="text/javascript">alert("아이디와 비밀번호를 입력해주세요!"); document.location.href="/";</script>');
-    }
+    // var username = req.body.username;
+    // var password = req.body.password;
+    // if (username && password) {
+    //     connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function (error, results, fields) {
+    //         if (results.length > 0) {
+    //             req.session.loggedin = true;
+    //             req.session.username = username;
+    //             res.send('<script type="text/javascript">alert("로그인에 성공하셨습니다!"); document.location.href="/stage";</script>');
+    //             res.end();
+    //         } else {
+    //             res.send('<script type="text/javascript">alert("ID 또는 비번이 틀렸습니다!"); document.location.href="/";</script>');
+    //         }
+    //         res.end();
+    //     });
+    // } else {
+    //     res.send('<script type="text/javascript">alert("아이디와 비밀번호를 입력해주세요!"); document.location.href="/";</script>');
+    // }
 });
 
 router.get('/stage', function (req, res) {
