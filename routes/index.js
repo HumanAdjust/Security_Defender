e59099784be10a23293afdd33d00436c1b53a6cc
H_Apple |A.S.A.P| <39252325+HumanAdjust@users.noreply.github.com>
2019-11-21 16:34:57 +0900

    .

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res){
     res.render('../views/index.ejs');
 });
 
+router.get('/login', function (req, res) {
+    res.render('../views/login.ejs');
+});
+
 module.exports = router;
