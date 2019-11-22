efba0d96aec84f635fdec5b7bcd9d1f4a6715749
Jong Geon Choi <54804037+zofqofhtltlm8015@users.noreply.github.com>
2019-11-22 11:27:42 +0900

    stage url만 넣으면 됩니다.
    stage url만 넣으면 됩니다.

--- public/js/Stage.js
+++ public/js/Stage.js
@@ -1,7 +1,6 @@
 
 let img = document.getElementById('Ship_comes_korea');
 var mood = 1;
-let k = Date();
 setInterval(function(){
     axios({
         method: 'POST',
@@ -10,10 +9,11 @@ setInterval(function(){
         {
             "mood" : mood
         },
-        k: Date()
+     
     }).then(function (response)
     {
-        $("#Time_comes_Korea").val(`위험 ${k} 에 어떤 배가 영토에 불법으로 들어갔습니다.`);
+        
+        $("#Time_comes_Korea").val(`위험! ${Date()} 에 어떤 배가 영토에 불법으로 들어갔습니다.`);
         console.log(response);
         console.log("data:image/gif;base64,"+ response.data);
         // =response.data
